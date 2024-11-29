da44c94dfcb3c68fa684f43a51777da7b6d701a9
Antonio Niño Díaz <antonio_nd@outlook.com>
2024-11-29 20:01:10 +0000

    Remove retry step when building blocksds-toolchain
    This is no longer needed apparently.

--- build.sh
+++ build.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 # build.sh by davidgfnet
 
-# Will build the specified packages.
-#
-# Example:
-#  ./build.sh sdl2 # Builds package SDL2
-#
-# On GitHub actions it is required to use "--retry" in the first package:
-#
-#  ./build.sh --retry sdl2
 
 set -e
 
 export PATH=/opt/wonderful/bin:$PATH
-
-doretry=""
-if [ "$1" == "--retry" ]; then
-    doretry="true"
-    shift
-fi
 
 if [ -z "$1" ]
 then
@@ -32,7 +18,4 @@ echo "Building $pkgdir ..."
 
 cd $pkgdir
 
-if [ ! -z "$doretry" ]; then
-    wf-makepkg -f --noconfirm || true
-fi
 wf-makepkg -f --noconfirm
